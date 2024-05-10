512a6e6668ce439f48238ed5e13050e6e20e399a
Andrey Sery <andrey.72006@gmail.com>
2024-05-10 20:31:28 +0300

    added createPost

--- express_api/controllers/post-controller.js
+++ express_api/controllers/post-controller.js
@@ -2,7 +2,27 @@ const { prisma } = require('../prisma/prisma-client');
 
 const PostController = {
   createPost: async (req, res) => {
-    res.send("createPost")
+    const { content } = req.body;
+
+    const authorId = req.user.userId;
+    if (!content) {
+      return res.status(400).json({ error: "Все поля обязательны" })
+    }
+
+    try {
+      const post = await prisma.post.create({
+        data: {
+          content,
+          authorId
+        }
+      })
+
+      res.json(post)
+    } catch (error) {
+      console.error("Create post error", error)
+
+      res.status(500).json({ error: "Internal server error" })
+    }
   },
   getAllPost: async (req, res) => {
     res.send("getAllPost")
