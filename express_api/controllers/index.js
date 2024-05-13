cb1907dd7db6a68e904843e7e951608ccba0198e
Andrey Sery <andrey.72006@gmail.com>
2024-05-13 23:16:27 +0300

    fix path avatarUrl. added controllers like and unlike

--- express_api/controllers/index.js
+++ express_api/controllers/index.js
@@ -1,9 +1,11 @@
 const PostController = require('./post-controller');
 const UserController = require('./user-controller');
 const CommentController = require('./comment-controller');
+const LikeController = require('./like-controller');
 
 module.exports = {
   UserController,
   PostController,
   CommentController,
+  LikeController,
 }
