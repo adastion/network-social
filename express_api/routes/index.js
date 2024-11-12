f293410c533e8a2e0b63d502fb89c8553aa414bb
Andrey Sery <andrey.72006@gmail.com>
2024-11-12 11:14:07 +0300

    restart

--- express_api/routes/index.js
+++ express_api/routes/index.js
@@ -1,44 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const { UserController, PostController, CommentController, LikeController, FollowsController } = require('../controllers');
-const authenticateToken = require('../middleware/auth');
+const express = require("express")
+const router = express.Router()
 
-const uploadDestination = 'uploads';
+router.get("/register", (req, res) => {
+  res.send("Buga!")
+})
 
-//showing where to store files
-const storage = multer.diskStorage({
-  destination: uploadDestination,
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  },
-});
-
-const uploads = multer({ storage: storage });
-
-// users routes
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
-router.get('/current', authenticateToken, UserController.current);
-router.get('/users/:id', authenticateToken, UserController.getUserById);
-router.put('/users/:id', authenticateToken, UserController.updateUser);
-
-// Posts routes
-router.post("/posts", authenticateToken, PostController.createPost);
-router.get("/posts", authenticateToken, PostController.getAllPosts);
-router.get("/posts/:id", authenticateToken, PostController.getPostById);
-router.delete("/posts/:id", authenticateToken, PostController.deletePost);
-
-// Comments routes
-router.post("/comments", authenticateToken, CommentController.createComment);
-router.delete("/comments/:id", authenticateToken, CommentController.deleteComment);
-
-// Likes routes
-router.post("/likes", authenticateToken, LikeController.likePost);
-router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
-
-// Follows routes
-router.post("/follows", authenticateToken, FollowsController.followUser);
-router.delete("/follows/:id", authenticateToken, FollowsController.unfollowUser);
-
-module.exports = router;
+module.exports = router
