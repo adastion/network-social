37e3521f024046f421af7733205f973d3e441b24
Andrey Sery <andrey.72006@gmail.com>
2024-11-28 15:01:47 +0300

    fixing path for folder "uploads" in app.js

--- back/app.js
+++ back/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('view engine', 'jade');
-app.use("uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"))
 
 app.use("/api", require("./routes"))
 
