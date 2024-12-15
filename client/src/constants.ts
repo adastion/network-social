2c52528086d6644093f4ae5652478aef2ae5abad
Andrey <63256342+adastion@users.noreply.github.com>
2024-12-15 15:36:10 +0300

    fixing constants.ts

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === "prodaction" ? "http://localhost:3000" : "http://localhost:3000"
+export const BASE_URL = process.env.NODE_ENV === "production" ? "http://localhost:3000" : "http://localhost:3000"
